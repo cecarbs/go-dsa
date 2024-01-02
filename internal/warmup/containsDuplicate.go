974fde0d9711a4e8442807c878122eff73c31996
cecarbs <carbonel257@gmail.com>
2024-01-01 20:48:18 -0600

    Added solution for containsDuplicate

---
+++ internal/warmup/containsDuplicate.go
@@ -0,0 +1,12 @@
+package warmup
+
+func containsDuplicate(nums []int) bool {
+	hashMap := make(map[int]bool)
+	for _, num := range nums {
+		if hashMap[num] {
+			return true
+		}
+		hashMap[num] = true
+	}
+	return false
+}
