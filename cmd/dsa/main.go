37a19d292abfbed5bd41ed070b871de667fcf084
cecarbs <carbonel257@gmail.com>
2024-01-02 20:10:37 -0600

    Added reverse vowels solution

--- cmd/dsa/main.go
+++ cmd/dsa/main.go
@@ -1,14 +1,8 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/cecarbs/dsa/internal/warmup"
-)
-
 func main() {
 	// sentence := "TheQuickBrownFoxJumpsOverTheLazyDog"
-	sentence := "This is not a pangram"
-	result := warmup.CheckIfPangram(sentence)
-	fmt.Println(result)
+	// sentence := "This is not a pangram"
+	// result := warmup.CheckIfPangram(sentence)
+	// fmt.Println(result)
 }
--- internal/warmup/reverseVowels.go
+++ internal/warmup/reverseVowels.go
@@ -1,14 +1,31 @@
 package warmup
 
+import "unicode"
+
 func ReverseVowels(str string) string {
+	strRunes := []rune(str)
 	i := 0
 	j := len(str) - 1
 
 	for i < j {
+		if !isVowelMap(rune(strRunes[i])) {
+			i++
+			continue
+		}
+		if !isVowelMap(rune(strRunes[j])) {
+			j--
+			continue
+		}
+
+		strRunes[i], strRunes[j] = strRunes[j], strRunes[i]
+		i++
+		j--
 	}
+	return string(strRunes)
 }
 
 func isVowelSlice(char rune) bool {
+	char = unicode.ToLower(char)
 	vowels := "aeiouAEIOU"
 	for _, vowel := range vowels {
 		return char == vowel
@@ -17,6 +34,7 @@ func isVowelSlice(char rune) bool {
 }
 
 func isVowelMap(char rune) bool {
+	char = unicode.ToLower(char)
 	vowelMap := map[rune]bool{
 		'a': true,
 		'e': true,
@@ -26,8 +44,5 @@ func isVowelMap(char rune) bool {
 	}
 
 	_, ok := vowelMap[char]
-	if ok {
-		return true
-	}
-	return false
+	return ok
 }
