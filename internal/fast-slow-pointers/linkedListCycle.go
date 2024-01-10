bcca88b453b459a93db6d6ce8930263273ef79d8
cecarbs <carbonel257@gmail.com>
2024-01-09 20:46:46 -0600

    Added solution for linkedlist cycle

---
+++ internal/fast-slow-pointers/linkedListCycle.go
@@ -0,0 +1,21 @@
+package fastslowpointers
+
+type ListNode struct {
+	Next *ListNode
+	Val  int
+}
+
+func hasCycle(head *ListNode) bool {
+	slow := head
+	fast := head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+
+		if slow == fast {
+			return true
+		}
+	}
+
+	return false
+}
